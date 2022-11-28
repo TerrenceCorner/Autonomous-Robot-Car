5d3be48a552566f499e426a3223e418ed147bdc6
Ciaran <ciaran@ciaranfahy.com>
2022-11-28 23:10:03 +0000

    update

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 //          Version 2.1, February 1999
 //          (for details see LICENSE file)
 //
-// Compilation: 
+// Compilation:
 // gcc -o testIR -Wall -Werror -lcurses -lwiringPi -lpthread -linitio testIR.c
 //
 //======================================================================
@@ -18,20 +18,20 @@
 // Drive forward and stop whenever an obstacle is detected by either
 // the left or right infrared (IR) sensor.
 //======================================================================
-void testIR(int argc, char *argv[]) 
+void testIR(int argc, char *argv[])
 {
 int ch = 0;
 
-while (ch != 'q') { 
+while (ch != 'q') {
         mvprintw(1, 1,"%s: Press 'q' to end program", argv[0]);
         if (initio_IrLeft()==0 && initio_IrRight()==0) {
                 // no obstacle ahead, so follow line
                 int lfL = initio_IrLineLeft();
                 int lfR = initio_IrLineRight();
-                if (lfL == 0 && lfR == 0) { 
+                if (lfL == 0 && lfR == 0) {
                 mvprintw(3, 1,"Action 2: Straight (Line sensors: %d, %d)    ", lfL, lfR);
                 // todo: move straight forward
-                initio_DriveForward (100);
+                initio_DriveForward (70);
                 }
                 else if (lfL == 0 && lfR == 1) {
                 // car is too much on the right
@@ -47,7 +47,6 @@ while (ch != 'q') {
                 }
                 else if(lfL == 1 && lfR == 1){
                 mvprintw(3, 1,"Lost my line (Line sensors: %d, %d)        ", lfL, lfR);
-
 
                 initio_DriveReverse(50);
                 delay(50);
@@ -73,19 +72,17 @@ return;
 //======================================================================
 int main (int argc, char *argv[])
 {
-	WINDOW *mainwin = initscr();  // curses: init screen
-	noecho ();                    // curses: prevent the key being echoed
-	cbreak();                     // curses: disable line buffering 
-	nodelay(mainwin, TRUE);       // curses: set getch() as non-blocking 
-	keypad (mainwin, TRUE);       // curses: enable detection of cursor and other keys
+        WINDOW *mainwin = initscr();  // curses: init screen
+        noecho ();                    // curses: prevent the key being echoed
+        cbreak();                     // curses: disable line buffering
+        nodelay(mainwin, TRUE);       // curses: set getch() as non-blocking
+        keypad (mainwin, TRUE);       // curses: enable detection of cursor and other keys
 
-	initio_Init (); // initio: init the library
+        initio_Init (); // initio: init the library
 
-	testIR(argc, argv);
+        testIR(argc, argv);
 
-	initio_Cleanup ();  // initio: cleanup the library (reset robot car)
-	endwin();           // curses: cleanup the library
-	return EXIT_SUCCESS;
+        initio_Cleanup ();  // initio: cleanup the library (reset robot car)
+        endwin();           // curses: cleanup the library
+        return EXIT_SUCCESS;
 }
-
-
